1e6a50f72f8a7c3a807b0996015bdd31a9c64339
bigveloper <bigveloper@gmail.com>
2021-09-25 21:32:07 +0900

    update App.jsx, Main.jsx

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,26 @@
+import Header from '../layout/Header';
+import Contents from '../layout/Contents';
+import Sidebar from '../layout/Sidebar';
+import Footer from '../layout/Footer';
+
 /**
  * @description Main
  */
 
 function Main() {
-    return;
+    const style = {
+        display: 'flex',
+    };
+    return (
+        <>
+            <Header title="Header" />
+            <div style={style}>
+                <Sidebar />
+                <Contents />
+            </div>
+            <Footer title="Footer" />
+        </>
+    );
 }
 
 export default Main;
