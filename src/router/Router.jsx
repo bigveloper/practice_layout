cc454a6a55700bdef6ebf71aca35feda411996bc
bigveloper <bigveloper@gmail.com>
2021-09-25 22:16:28 +0900

    Router.jsx

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,18 @@
+import { Route, Switch } from 'react-router-dom';
 /**
  * @description Router
  */
 
 function Router() {
-    return;
+    return (
+        <Switch>
+            <Route path="menu01" component={Menu01} />
+            <Route path="menu02" component={Menu02} />
+            <Route path="menu03" component={Menu03} />
+            <Route path="menu04" component={Menu04} />
+            <Route path="menu05" component={Menu05} />
+        </Switch>
+    );
 }
 
 export default Router;
