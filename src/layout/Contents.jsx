dae11d0e70b98be288a9328f781f0f7298b879f3
bigveloper <bigveloper@gmail.com>
2021-09-25 22:09:20 +0900

    Contents.jsx

--- src/layout/Contents.jsx
+++ src/layout/Contents.jsx
@@ -1,9 +1,23 @@
+import { Router } from 'react-router-dom';
 /**
  * @description Contents
  */
 
 function Contents() {
-    return;
+    const style = {
+        display: 'flex',
+        alignItem: 'center',
+        justifyContent: 'center',
+
+        width: '85%',
+        height: 'calc(100vh-200px)',
+        border: '1px solid black',
+    };
+    return (
+        <div style={style}>
+            <Router />
+        </div>
+    );
 }
 
 export default Contents;
