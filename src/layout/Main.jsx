f24589f0ee3ebae6f0cc2ef1de24b1cb16fb263a
bigveloper <bigveloper@gmail.com>
2021-09-25 12:06:05 +0900

    update jsx

---
+++ src/layout/Main.jsx
@@ -0,0 +1,23 @@
+import Header from '../layout/Header';
+import Footer from '../layout/Footer';
+import Sidebar from '../layout/Sidebar';
+import Contents from '../layout/Contents';
+
+/**
+ * @description Main
+ */
+
+function Main() {
+    return (
+        <>
+            <Header title="Header" />
+            <div style={style}>
+                <Sidebar />
+                <Contents />
+            </div>
+            <Footer title="Footer" />
+        </>
+    );
+}
+
+export default Main;
---
+++ src/router/Router.jsx
@@ -0,0 +1,11 @@
+import { Route, Switch } from 'react-router-dom';
+
+/**
+ * @description Router
+ */
+
+function Router() {
+    return;
+}
+
+export default Router;
