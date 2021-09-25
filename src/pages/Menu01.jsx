7fdc53e9a88118dd037959006723c6e009c35418
bigveloper <bigveloper@gmail.com>
2021-09-25 18:19:34 +0900

    all clear practice_layout

--- src/pages/Menu01.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * @description 메뉴01
- */
-
-function Menu01() {
-    return <h1>Menu01</h1>;
-}
-
-export default Menu01;
--- src/pages/Menu02.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * @description 메뉴02
- */
-
-function Menu02() {
-    return <h1>Menu02</h1>;
-}
-
-export default Menu02;
