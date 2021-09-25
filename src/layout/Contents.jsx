f5caa978bc932383592ca8115d259d1f75ec0a05
bigveloper <bigveloper@gmail.com>
2021-09-26 00:17:09 +0900

    Contents.jsx

--- src/layout/Contents.jsx
+++ src/layout/Contents.jsx
@@ -1,9 +1,24 @@
+import Router from '../router/Router';
+
 /**
  * @description Contents
  */
 
 function Contents() {
-    return;
+    const style = {
+        display: 'felx',
+        alignItem: 'center',
+        justifyContent: 'center',
+
+        width: '85%',
+        height: 'calc(100vh - 200px)',
+        border: '3px solid green',
+    };
+    return (
+        <div style={style}>
+            <Router />
+        </div>
+    );
 }
 
 export default Contents;
