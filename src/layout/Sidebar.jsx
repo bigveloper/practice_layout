3d21debb87657b09c45806d3d487e62fdb1d2273
bigveloper <bigveloper@gmail.com>
2021-09-26 12:34:07 +0900

    Sidebar.jsx

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,9 +1,40 @@
+import { Link } from 'react-router-dom';
+
 /**
  * @description Sidebar
  */
 
 function Sidebar() {
-    return;
+    const style = {
+        display: 'flex',
+        justfyContent: 'center',
+
+        width: '85%',
+        height: 'calc(100vh - 200px)',
+        border: '3px solid red',
+        boxSizing: 'border-box',
+    };
+    return (
+        <div style={style}>
+            <ul>
+                <li>
+                    <link to="/menu01">Menu01</link>
+                </li>
+                <li>
+                    <link to="/menu02">Menu02</link>
+                </li>
+                <li>
+                    <link to="/menu03">Menu03</link>
+                </li>
+                <li>
+                    <link to="/menu04">Menu04</link>
+                </li>
+                <li>
+                    <link to="/menu05">Menu05</link>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default Sidebar;
