c8abefa3a8f412a186087e0185b3c9cf47feb22f
bigveloper <bigveloper@gmail.com>
2021-09-26 15:23:22 +0900

    checkbox

--- src/pages/Menu02.jsx
+++ src/pages/Menu02.jsx
@@ -1,9 +1,34 @@
-/**
- * @description Menu02
- */
+import { useState, useEffect } from 'react';
 
 function Menu02() {
-    return <h1>Menu02</h1>;
+    const [laptop, setLaptop] = useState('');
+
+    const onChange = (e) => setLaptop(e.target.value);
+
+    useEffect(() => {
+        console.log(laptop);
+    }, [laptop]);
+
+    return (
+        <form>
+            <h1>What do you want to laptop?</h1>
+            <input type="checkbox" name="laptop" value="MacBookAir" onChange={onChange} />
+            MacBook Air
+            <br />
+            <input type="checkbox" name="laptop" value="MacBookPro" onChange={onChange} />
+            MacBook Pro
+            <br />
+            <input type="checkbox" name="laptop" value="GalaxyBook" onChange={onChange} />
+            GalaxyBook
+            <br />
+            <input type="checkbox" name="laptop" value="MacBookAir" onChange={onChange} />
+            ZenBook
+            <br />
+            <input type="submit" />
+            <br />
+            <input type="reset" />
+        </form>
+    );
 }
 
 export default Menu02;
